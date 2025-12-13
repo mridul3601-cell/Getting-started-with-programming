3ae70850000c7182d217f140d9f8bbbdc9774689
mridul3601-cell <Mridul3601@gmail.com>
2025-12-13 18:49:44 -0500

    v2

---
+++ Activity2.py
@@ -0,0 +1,10 @@
+x=5 
+y = "Mridul"
+print (x)
+print (y)
+Grade = 8
+greeting = "good morning"
+print (Grade)
+print (greeting)
+Name = input ('please enter your name') 
+print ('Hey',Name,'welcome to the coding world')
